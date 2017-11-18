7bfdbcefc2691925a8d44e0e9fa210b4e9fb2d85
Abhishek Goyal <abhishekgoyal@Abhisheks-MacBook-Pro.local>
2017-11-18 21:03:09 +0530

    Randon number generated for both the dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var diceValueLeft : Int = 0;
+    var diceValueRight : Int = 0;
     @IBOutlet weak var diceImageViewRight: UIImageView!
     @IBOutlet weak var diceImageViewLeft: UIImageView!
     
-    @IBAction func rollMeButtonPresses(_ sender: UIButton) {
+    @IBAction func rollMeBtnPressed(_ sender: UIButton) {
+        
+        diceValueLeft = Int(arc4random_uniform(6))+1;
+        diceValueRight = Int(arc4random_uniform(6)+1);
+        
+        print(diceValueLeft);
+        print(diceValueRight);
         
         
         
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
