673025949b103eadaafd87fa03b1d244a0da3d39
Abhishek Goyal <abhishekgoyal@Abhisheks-MacBook-Pro.local>
2017-11-18 20:33:41 +0530

    Image View added for Bacjground,Logo and Both the Dice's

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Abhishek Goyal on 17/11/17.
+//  Copyright © 2017 Abhishek Goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    
+    @IBAction func rollMeButtonPresses(_ sender: UIButton) {
+        
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
