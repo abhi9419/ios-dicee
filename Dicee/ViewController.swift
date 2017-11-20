4e15578b086bc1aa50e378f8cecf753453fc6cdb
Abhishek Goyal <abhishekgoyal@Abhisheks-MacBook-Pro.local>
2017-11-20 12:33:56 +0530

    Dice Image changes based on random number

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -15,13 +15,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewRight: UIImageView!
     @IBOutlet weak var diceImageViewLeft: UIImageView!
     
+    let diceImagesArray  = ["dice1","dice2","dice3","dice4","dice5","dice6"];
+    
     @IBAction func rollMeBtnPressed(_ sender: UIButton) {
         
-        diceValueLeft = Int(arc4random_uniform(6))+1;
-        diceValueRight = Int(arc4random_uniform(6)+1);
+        diceValueLeft = Int(arc4random_uniform(6));
+        diceValueRight = Int(arc4random_uniform(6));
         
-        print(diceValueLeft);
-        print(diceValueRight);
+        //print(diceValueLeft);
+        //print(diceValueRight);
+        
+        diceImageViewLeft.image = UIImage(named:diceImagesArray[diceValueLeft]);
+        diceImageViewRight.image = UIImage(named:diceImagesArray[diceValueRight]);
         
         
         
